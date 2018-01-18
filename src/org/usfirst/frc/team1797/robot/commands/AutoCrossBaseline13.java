9db2beda017b8454f419489ef28903cd85344eaf
LordSquishers <patrick_asl@25.32.198.39>
2018-01-18 18:10:09 +0000

    Added code for AutoCrossBaseline13

--- src/org/usfirst/frc/team1797/robot/commands/AutoCrossBaseline13.java
+++ src/org/usfirst/frc/team1797/robot/commands/AutoCrossBaseline13.java
@@ -9,6 +9,11 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class AutoCrossBaseline13 extends Command {
 
+	private static final double DRIVE_LENGTH = 2; //the time for the command to run for (in seconds)
+	private static final double DRIVE_SPEED = 0.3; // Speed for motors to run at while executing
+	
+	private long startTime = Long.MAX_VALUE; //Similar to last actuation but used for timing motors.
+	
     public AutoCrossBaseline13() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -17,6 +22,8 @@ public class AutoCrossBaseline13 extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    		startTime = System.currentTimeMillis();
+    		Robot.DRIVE_TRAIN.arcadeDrive(DRIVE_SPEED, 0);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -25,15 +32,17 @@ public class AutoCrossBaseline13 extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return System.currentTimeMillis() - startTime >= (DRIVE_LENGTH * 1000);
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    		Robot.DRIVE_TRAIN.stopDrive();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    		end();
     }
 }
