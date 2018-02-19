6e491860b80a0e76bc3ced9fa498571369453345
Arman <armanbhalla6@gmail.com>
2018-02-19 11:04:55 +0000

    Added p.o.c. of deliverboxrtor

--- src/org/usfirst/frc/team1797/robot/auto/AutoDeliverBoxRtoR.java
+++ src/org/usfirst/frc/team1797/robot/auto/AutoDeliverBoxRtoR.java
@@ -2,27 +2,24 @@ package org.usfirst.frc.team1797.robot.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
+import org.usfirst.frc.team1797.robot.Robot;
+import org.usfirst.frc.team1797.robot.commands.FlipCommand;
+import org.usfirst.frc.team1797.robot.commands.auto.autoutils.*;;
+
 /**
- *
+ * @author techtide
+ * @see The path of the different autonomous routines, on the Github routine.
  */
+
 public class AutoDeliverBoxRToR extends CommandGroup {
-
-    public AutoDeliverBoxRToR() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    }
+	public AutoDeliverBoxRToR() {
+		// Move forward ~150 units till the robot is past the switch.
+		addSequential(new MoveStraight(Direction.FORWARD, 0.5, 150));
+		// Now, turn at a right angle and hit the fence opposite the switch to wall align with it.
+		addSequential(new TurnCommand(Direction.RIGHT, 0.5, 90));
+		// Reverse so the robot hits the fence.
+		addSequential(new MoveStraight(Direction.BACKWARD, 0.5, 85.25));
+		// Deliver the box and throw it backwards.
+		addSequential(new FlipCommand());
+	}
 }
